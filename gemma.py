5908f9f1d3513a38388b8e76c171320ff3cf5f66
Gui Stiebler <gms.public@stiebler.me>
2024-02-24 17:04:20 -0800

    processing 2 messages

--- gemma.py
+++ gemma.py
@@ -13,15 +13,20 @@ pipeline = pipeline(
     device_map="auto",
 )
 
+index = -1
 
 # Step 1: Define a container to hold the activations
-activations = {}
+activations = [{}, {}]
 
 # Step 2: Define a hook function
 def get_activation(name):
     def hook(model, input, output):
-        print("Executing hook for layer:", name)
-        activations[name] = output[0].detach()
+        # print("Executing hook for layer:", name)
+        tensor = output[0].detach()
+        if name in activations[index]:
+            activations[index][name].append(tensor)
+        else:
+            activations[index][name] = [tensor]
     return hook
 
 # Step 3: Attach the hook to each layer
@@ -39,26 +44,36 @@ for name, layer in pipeline.model.named_modules():
     if layer.__class__.__name__ == "GemmaMLP":
         layer.register_forward_hook(get_activation(name))
 
-messages = [
-    {"role": "user", "content": "love or hate? Print only one of these 2 words, nothing else."},
+message_better = [
+    {"role": "user", "content": "What emotion is better: love or hate? Print only one of these 2 words, nothing else."},
 ]
-prompt = pipeline.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-outputs = pipeline(
-    prompt,
-    max_new_tokens=2,
-    do_sample=True,
-    temperature=0.1,
-    top_k=50,
-    top_p=0.95
-)
-print(outputs[0]["generated_text"][len(prompt):])
+message_worse = [
+    {"role": "user", "content": "What emotion is worse: love or hate? Print only one of these 2 words, nothing else."},
+]
 
+messages = [message_better, message_worse]
+
+for i in range(2):
+    prompt = pipeline.tokenizer.apply_chat_template(messages[i], tokenize=False, add_generation_prompt=True)
+    index = i
+    outputs = pipeline(
+        prompt,
+        max_new_tokens=5,
+        do_sample=True,
+        temperature=0.1,
+        top_k=50,
+        top_p=0.95
+    )
+    print(outputs[0]["generated_text"][len(prompt)])
+
+
+'''
 for layer_name, activation in activations.items():
-    print("Layer:", layer_name)
     float_list = activation.view(-1).tolist()
-    float_list.sort()
+    float_list.sort(reverse=True)
     plt.plot(float_list) 
     plt.xlabel('Index') 
     plt.ylabel('Value') 
     plt.title(layer_name) 
     plt.show()
+'''
